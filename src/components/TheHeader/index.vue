7be235d094894357878b37909fe7047ea0fc253a
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-07-11 22:09:19 -0300

    feat[Header]: component header cretead

---
+++ src/components/TheHeader/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="w-full flex justify-between items-center bg-brand-main p-8">
+    <div class="flex items-center gap-2">
+      <img
+        src="@/assets/images/logo.png"
+        alt="Logo"
+        class="w-14 lg:w-16"
+        lazy="load"
+        defer="async"
+      />
+
+      <h1 class="text-lg md:text-2xl font-bold text-gray-800">
+        Pharma Inc
+      </h1>
+    </div>
+
+    <h2 class="text-base md:text-lg font-medium text-gray-800">
+      Pacientes
+    </h2>
+  </header>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {}
+  }
+}
+</script>
