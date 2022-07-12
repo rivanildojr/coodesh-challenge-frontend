df52b6148108601f5b853e9dbfbc0c6dcd119adc
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-07-11 21:00:10 -0300

    config[]: install dependencies

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Toast, { POSITION } from 'vue-toastification'
+
+import 'animate.css'
+import '@/assets/css/tailwind.css'
+import '@/assets/css/font.css'
+import 'vue-toastification/dist/index.css'
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(Toast, { position: POSITION.TOP_RIGHT })
+app.mount('#app')
