19c667c10996d78e3b5c3deb48a4c9b1033063ef
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-07-13 01:28:38 -0300

    feat[Service]: added request get one user

--- src/services/Patient/index.js
+++ src/services/Patient/index.js
@@ -22,5 +22,22 @@ export default httpClient => ({
       data: response.data,
       errors
     }
+  },
+  getOne: async ({ id }) => {
+    const response = await httpClient.get(`/?id=${id}`)
+
+    let errors = null
+
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.statusText
+      }
+    }
+
+    return {
+      data: response.data,
+      errors
+    }
   }
 })
