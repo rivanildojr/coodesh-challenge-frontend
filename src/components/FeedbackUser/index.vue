48bf5f4ee5e6cd50ff5edfb096055541a16e78db
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2022-07-13 21:27:49 -0300

    feat[FeedbackUser]: created component feedbackUser

---
+++ src/components/FeedbackUser/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <p
+    :class="{'text-lg font-bold text-gray-800 text-center': message}"
+  >
+    <template v-if="message">{{ message }}</template>
+    <slot v-else />
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
